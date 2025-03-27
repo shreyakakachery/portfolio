97897a9fd5ed9016ece98a41f2807194db3a158b
Shreya Kakachery <shreyakakachery@gmail.com>
2025-03-27 12:28:07 -0700

    Add projects list

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from './components/Header/Header.jsx'
+import Projects from './components/Projects/Projects.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import Footer from './components/Footer/Footer.jsx'
 import './App.scss'
@@ -9,6 +10,7 @@ function App() {
     <div>
       <p>App.jsx</p>
       <Header />
+      <Projects />
       <Contact />
       <Footer />
     </div>
---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,51 @@
+function Projects() {
+  return (
+    <div>
+      <p>Projects.jsx</p>
+      <section>
+        <h3>Software Engineering</h3>
+        <ul>
+          <li>
+            <h4>Convene</h4>
+            <p>transit based places suggestions</p>
+          </li>
+          <li>
+            <h4>OneVoice</h4>
+            <p>accented speech transcription tool</p>
+          </li>
+          <li>
+            <h4>Hike Buddy</h4>
+            <p>Hiking trails visualizer</p>
+          </li>
+          <li>
+            <h4>Polaroid Gif</h4>
+            <p>Gif caption maker</p>
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h3>Neuroscience</h3>
+        <ul>
+          <li>
+            <h4>ToM Autism</h4>
+            <p>Theory of Mind and Autism SPectrum Disorder</p>
+          </li>
+          <li>
+            <h4>ToM Ageing</h4>
+            <p>Theory of Mind and Ageing/ Neurodegeneration</p>
+          </li>
+          <li>
+            <h4>Connectime-based Predictive Modelling</h4>
+            <p>fmri</p>
+          </li>
+          <li>
+            <h4>Creative Mind</h4>
+            <p>AHDH and Creativity</p>
+          </li>
+        </ul>
+      </section>
+    </div>
+  );
+}
+
+export default Projects;
